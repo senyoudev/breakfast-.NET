19cdcfe31e677c18064b1f1c47a01732b9293ecc
senyoudev <younesmeskafe2018@gmail.com>
2023-10-11 13:24:48 +0100

    Add project files.

---
+++ breakfast/Controllers/BreakFastController.cs
@@ -0,0 +1,22 @@
+﻿using breakfastapp.contracts.Breakfast;
+using Microsoft.AspNetCore.Mvc;
+
+namespace breakfast.Controllers;
+
+    [ApiController]
+    public class BreakFastController : ControllerBase
+    {
+        [HttpPost("/breakfasts")]
+        public IActionResult CreateBreakFast(CreateBreakfastRequest request)
+        {
+            return Ok(request);
+        }
+
+        [HttpGet("/breakfasts/{id:guid}")]
+        public IActionResult GetBreakFAst(Guid id)
+        {
+            return Ok(id);
+        }
+
+}
+
---
+++ breakfastapp.contracts/Breakfast/BreakFastResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace breakfastapp.contracts.Breakfast;
+    public record BreakfastResponse(
+        Guid Id,
+        string Name,
+        string Description,
+        DateTime StartDateTime,
+        DateTime EndDateTime,
+        DateTime LastModifiedDateTime,
+        List<string> Savory,
+        List<string> Sweet
+       );
